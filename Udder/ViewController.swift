20ce17dce9295588c7a1314473a3b12ecbdf26b5
Peter Mayne <petermayne@2405-9800-bc10.44.pool1.sila1-bcr02.myaisfibre.com>
2018-09-20 16:13:21 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Peter Mayne on 20/09/2018.
+//  Copyright © 2018 Mayne Music Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
